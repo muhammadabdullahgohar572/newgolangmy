46aede13cd4ff6780370a0fc0fdbdd9584174df8
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-11-15 03:27:06 +0500

    first commit

---
+++ Handler.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const mongoURI = "mongodb+srv://Abdullah:Abdullah@cluster0.6zuld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+func Dbconnect() (*mongo.Client,error) {
+	
+}
+
+func main() {
+	fmt.Print("USER CREATE")
+	
+
+}
